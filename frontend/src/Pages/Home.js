2abc55747e39b7537554d7610ae72501a8a6ef90
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-08-14 12:12:15 +0100

    Fetching and Displaying data start
    useEffect hook for fetching API data
    Component made to pull data from the db and make a workout card

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,7 +1,34 @@
+import { useEffect, useState} from 'react'
+
+//components
+import WorkoutDetails from '../Components/WorkoutDetails'
+
+
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    //Fetching data from API
+    useEffect(() => {
+        const fetchWorkouts = async() => {
+            const response = await fetch('/api/workouts') //fetch the endpoint of the API URL
+            const json = await response.json()
+
+            if (response.ok){
+                setWorkouts(json)
+            }
+        }
+        fetchWorkouts()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className='workouts'>
+               {workouts && workouts.map ( (workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+               ) )} 
+            </div>
         </div>
     )
 }
