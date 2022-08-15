ec8ae0b32b48e5da58f5416f5bbd1b073368fbe6
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-08-15 13:17:03 +0100

    React Context updates
    Adding React Contexts.
    Once Form is submitted, new exercise will appear as entry instead of needing to refresh page.
    
    Keeping our frontend data/state in sync with the database using React Context.

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,38 +1,36 @@
-import { useEffect, useState} from 'react'
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
-//components
-import WorkoutDetails from '../Components/WorkoutDetails'
-import WorkoutForm from '../Components/WorkoutForm';
-
-
+// components
+import WorkoutDetails from "../Components/WorkoutDetails"
+import WorkoutForm from "../Components/WorkoutForm"
 
 const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext()
 
-    const [workouts, setWorkouts] = useState(null)
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
 
-    //Fetching data from API
-    useEffect(() => {
-        const fetchWorkouts = async() => {
-            const response = await fetch('/api/workouts') //fetch the endpoint of the API URL
-            const json = await response.json()
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
 
-            if (response.ok){
-                setWorkouts(json)
-            }
-        }
-        fetchWorkouts()
-    }, [])
+    fetchWorkouts()
+  }, [dispatch])
 
-    return (
-        <div className="home">
-            <div className='workouts'>
-               {workouts && workouts.map ( (workout) => (
-                    <WorkoutDetails key={workout._id} workout={workout}/>
-               ) )} 
-            </div>
-            <WorkoutForm />
-        </div>
-    )
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  )
 }
 
 export default Home
