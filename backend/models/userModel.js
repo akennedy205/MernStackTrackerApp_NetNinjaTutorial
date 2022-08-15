03c0d0985af691a094237be2a176031259014fe7
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-08-15 19:16:54 +0100

    Login and email validation
    JWT login validation for email and password fields working and tested with Postman

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,51 +2,66 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const validator = require('validator')
 
-//creating Schema for usernames and passwords
-const Schema= mongoose.Schema
-const userSchema = new Schema ({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 })
 
-//Static method for SignUp.
-userSchema.statics.signup = async function(email, password){
-    //validation
+// static signup method
+userSchema.statics.signup = async function(email, password) {
 
-    if (!email || !password){ //Check if email and password fields are NOT filled
-        throw Error('All fields must be filled')
-    }
-    if(!validator.isEmail(email)){
-        throw Error('Email is not valid. Please enter a different email') //Check if email is actually an email
-    }
-    if (!validator.isStrongPassword(password)){
-        throw Error('Password not strong enough') //Check if password is strong enough
-    }
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
 
+  const exists = await this.findOne({ email })
 
-   const exists = await this.findOne({email}) //check if email already exists
-
-   if (exists){
+  if (exists) {
     throw Error('Email already in use')
-   }
+  }
 
-   //Password salting and hashing
-   const salt = await bcrypt.genSalt(10)
-   const hash = await bcrypt.hash(password, salt)
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
 
-   const user = await this.create({email, password: hash})
+  const user = await this.create({ email, password: hash })
 
-   return user
+  return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password) {
 
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
+
+  return user
+}
 
 module.exports = mongoose.model('User', userSchema)
-//export to userController.js
