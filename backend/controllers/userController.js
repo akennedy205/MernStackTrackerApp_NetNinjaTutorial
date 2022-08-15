d7096aecdd1995de271cc87c53d67fe6ffadb2a9
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-08-15 18:53:11 +0100

    JSON Web Tokens
    JSON Web Tokens added to handle the authentication between the front and backend.
    
    Token generation tested on PostMan

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,13 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+//Function creation to generate tokens
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
+}
+
+
+
 
 
 //login user=> 
@@ -16,7 +25,11 @@ const signupUser = async (req, res) => {
 
     try{
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+
+        //create token
+        const token = createToken(user._id);
+
+        res.status(200).json({email, token})
     } catch (error){
         res.status(400).json({error: error.message})
     }
