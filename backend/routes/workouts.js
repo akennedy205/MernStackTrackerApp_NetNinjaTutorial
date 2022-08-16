a9d594b722c3ac428dd6073289c2731b03e6b6ad
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-08-16 19:20:20 +0100

    Protecting API Routes
    Putting in middleware. Now API requests  to get exercises from DB will only work if token authentication exists.

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,7 +2,10 @@ const express = require('express') //requiring express
 const router = express.Router()//Setting up Express Router
 
 const { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout } = require('../controllers/workoutController') //Importing the controller functions
-const { update } = require('../models/workoutModel')
+const requireAuth = require('../middleware/requireAuth')
+
+//require auth for all workout routes
+router.use(requireAuth)
 
 //GET all workouts
 router.get('/', getWorkouts)
